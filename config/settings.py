b8bc788688be4991bd14695497a4a1ccab1da9c7
ozodbek_7765 <programmer7765@gmail.com>
2025-03-11 01:14:28 +0500

    Update .gitignore and add changes

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
