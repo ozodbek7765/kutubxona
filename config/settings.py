b2210d02d1afb75660f4e40aa091bb369fc41276
ozodbek_7765 <programmer7765@gmail.com>
2025-03-11 12:03:22 +0500

    Update settings for Railway deployment

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default='postgresql://postgres:mdHZxigADxNyJSmEetoBaWDOggBAzxiX@nozomi.proxy.rlwy.net:14625/railway',
+        engine='django.db.backends.postgresql'
+    )
 }
 
 
